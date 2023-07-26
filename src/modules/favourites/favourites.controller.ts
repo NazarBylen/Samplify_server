218f3ae9cc3cb30a0412ea0da7008abba2313f57
unknown <nazar.bylen.ir.2021@lpnu.ua>
2023-07-26 20:47:20 +0300

    naming refactored

--- src/modules/favourites/favourites.controller.ts
+++ src/modules/favourites/favourites.controller.ts
@@ -7,11 +7,11 @@ favouritesController.get("/", async function(req, res, next) {
     return await favouritesService.getAllFavourites(req, res, next);
 })
 
-favouritesController.get("/getFavourite/:userId", async function(req, res, next) {
+favouritesController.get("/:userId", async function(req, res, next) {
     return await favouritesService.getFavouriteSongsById(req, res, next);
 })
 
-favouritesController.post("/addFavourite", async function (req, res,next){
+favouritesController.post("/", async function (req, res,next){
     return await favouritesService.saveToFavourites(req, res, next);
 })
 

2e102f9ec9e01fef188662ff7e9ccb86099d7dc8
unknown <nazar.bylen.ir.2021@lpnu.ua>
2023-12-22 02:17:20 +0200

    nest

--- utils/jwt.ts
+++
@@ -1,9 +0,0 @@
-import * as jwt from "jsonwebtoken";
-
-export function generateAccessToken(user) {
-  return jwt.sign(user, 'ACCESS_TOKEN_SECRET', {expiresIn: "60m"})
-}
-
-export function generateRefreshToken(user) {
-  return jwt.sign(user, 'REFRESH_TOKEN_SECRET', { expiresIn: "120m" })
-}
